0c243a3e3fdae97f30051b1239a86f00e256a4a8
Gauravmanali <marcosgaurav1234@gmail.com>
2023-04-02 12:31:32 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(name="cencus-income",
+version= 0.01,
+author='Gaurav Manali',
+author_email='marcosgaurav1234@gmail.com',
+packages = find_packages(),
+install_requires=["pandas","numpy","flask"]
+)
